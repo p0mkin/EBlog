b19cadda14a9b69c81d2d02428bb4491996910ad
p0mkin <77664722+p0mkin@users.noreply.github.com>
2026-02-23 00:33:42 +0200

    Desktop Video Stretching Fix & Loading Bars

--- src/app/signin/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-[#0a0a0a]">
-            <div className="w-6 h-6 border-2 border-white/20 border-t-white rounded-full animate-spin" />
-        </div>
-    );
-}
